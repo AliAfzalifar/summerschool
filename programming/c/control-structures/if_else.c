0f6c4a831cb0ff4f8a057f247fa26775cde3e806
Aaf <ali.afzalifar84@gmail.com>
2018-06-27 12:08:16 +0300

    if_else solved

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -7,5 +7,26 @@ int main(int argc, char *argv[])
     // write the control structure as described in the exercise
     // and run the code with different values for i
 
+    int N = 3, k;
+
+    for (k=0; k<=N+1 ;k++) {
+      
+      switch(k) {
+      case 1:
+	printf("case1 %d \n", k);
+	break;
+      case 2:
+	printf("case2 %d \n", k);
+	break;
+      case 3:
+	printf("case3 %d \n", k);
+	break;
+      default:
+	printf("default %d \n", k);
+	break;
+      }
+    }
+      
+
     return 0;
 }
